5df307d7b1539f0603f43d56e4231e2d9486a1ae
gooneraki <hayiate4@hotmail.com>
2025-10-15 12:39:37 +0300

    feat: implement health check endpoint and add initial HTML structure

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.get("/healthz", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send("OK");
+});
+
+// app.use(express.static("./app/"));
+app.use("/app", express.static("./src/app"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
