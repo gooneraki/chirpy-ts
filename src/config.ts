0b26b403228ff2742f7de0420c582f5e435c6f94
gooneraki <hayiate4@hotmail.com>
2025-10-17 16:44:08 +0300

    feat: add environment configuration and database connection setup

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing key in env: '${key}'`);
+  }
+  return value;
+}
+
 type APIConfig = {
   fileServerHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileServerHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
